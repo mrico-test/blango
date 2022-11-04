82454d9b432bc0ca909fe84c2a3d3494392c758e
Maria Morillo <mricomorillo@gmail.com>
2022-11-04 17:09:07 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -37,11 +37,12 @@ class Dev(Configuration):
       'django.contrib.sessions',
       'django.contrib.messages',
       'django.contrib.staticfiles',
+      'blango_auth',
       'blog',
       'crispy_forms', 
       'crispy_bootstrap5',
       'debug_toolbar',
-      
+
   ]
 
   MIDDLEWARE = [
@@ -145,6 +146,8 @@ class Dev(Configuration):
   CRISPY_TEMPLATE_PACK = "bootstrap5"
 
   INTERNAL_IPS = ["192.168.10.93"]
+
+  AUTH_USER_MODEL = "blango_auth.User"
 
 
 class Prod(Dev):
